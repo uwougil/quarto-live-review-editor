9366ddd339eaf9ea6777c5337fd465c7ec11b75b
guoli <uwougil@users.noreply.github.com>
2026-09-13 23:33:43 +0800

    feat: refine default typewriter mode

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,7 @@ import { StyleStore } from './sidebar/styleStore';
 import { OutlineViewProvider } from './sidebar/OutlineViewProvider';
 import { setGrammarRoot } from './editor/shikiHost';
 import { runDocumentSyncIntegration } from './editor/documentSyncIntegration';
+import { DEFAULT_TYPEWRITER_MODE } from './shared/typewriterMode';
 import {
 	reconcileInspectedEditorAssociations,
 	type DefaultEditorMode,
@@ -163,7 +164,7 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
 	const { disposable: providerDisposable, provider } = MarkdownLivePreviewProvider.register(
 		context,
 		() => styleStore.getCombinedCssSync(),
-		() => vscode.workspace.getConfiguration('mdLivePreview').get<boolean>('typewriterMode', false),
+		() => vscode.workspace.getConfiguration('mdLivePreview').get<boolean>('typewriterMode', DEFAULT_TYPEWRITER_MODE),
 		() => vscode.workspace.getConfiguration('mdLivePreview').get<boolean>('normalizeMathOnPaste', false),
 	);
 	context.subscriptions.push(providerDisposable);
